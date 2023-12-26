8819636dec587750a82d78569b7a5ff20475d240
dereknguyenm <48599167+dereknguyenm@users.noreply.github.com>
2023-12-25 18:08:47 -0800

    Add Initial

---
+++ prompts.py
@@ -0,0 +1,49 @@
+# 1) Two Sum
+#    Write a function that takes in a non-empty array of distinct integers and an
+#    integer representing a target sum. If any two numbers in the input array sum
+#    up to the target sum, the function should return them in an array, in any
+#    order. If no two numbers sum up to the target sum, the function should return
+#    an empty array.
+
+
+# 2) Validate Subsequence
+#    Given two non-empty arrays of integers, write a function that determines whether
+#    the second array is a subsequence of the first one.
+#    A subsequence of an array is a set of numbers that aren't necessarily adjacent in the array
+#    but that are in the same order as they appear in the array.For instance, the numbers
+
+
+# 3) Sorted Squared Array
+#    Write a function that takes in a non-empty array of integers that are sorted in ascending order
+#    and returns a new array of the same length with the squares of the original integers also sorted
+#    in ascending order.
+
+
+# 4) Tournament Winner
+#    There's an algorithm tournament taking place in which teams of programmers compete against each other
+#    to solve algorithmic problems as fast as possible. Teams compete in a roundrobin, where each team
+#    faces off against all other teams. Only two teams compete against each other at a time, and for each
+#    competition, one team is designated the home team, while the other team is the away team. In each
+#    competition there's always one winner and one loser; there are no ties. A team receives 3 points if it
+#    wins and 0 points if it loses. The winner of the tournament is the team that receives the most amount
+#    of points.
+
+
+# 5) Non-Constructable
+#    Given an array of positive integers representing the values of coins in your possession,
+#    write a function that returns the minimum amount of change (the minimum sum of money) that you
+#    cannot create. The given coins can have any positive integer value and aren't necessarily unique
+#    (i.e., you can have multiple coins of the same value)
+#    For example, if you're given [1,2,5], the minimum amount of change that you can't create is 4
+
+
+# 6) Transpose Matrix
+#    You're given a 2D array of integers matrix. Write a function that returns the transpose of the matrix.
+#    The transpose of a matrix is a flipped version of the original matrix across its main diagonal
+#    (which runs from top-left to bottom-right); it switches the row and column indices of the original
+#    matrix.You can assume the input matrix always has at least 1 value; however its width and height are
+#    not necessarily the same.
+
+
+
+
---
+++ solutions.py
@@ -0,0 +1,150 @@
+# 1) TWO SUM
+# Solution 1
+def twoNumberSum(array, targetSum):
+    # Using Hashing with O(n) run time and O(n) space complexity
+    values = {}
+    for element in array:
+
+        sumValue = targetSum - element  # 10-3
+
+        if sumValue in values:
+            return [element, sumValue]
+        else:
+            values[element] = True
+    return []
+
+
+# Solution 2
+def twoNumberSum2(array, targetSum):
+    # runtime of nlog(n)
+    array.sort()
+    left = 0
+    right = len(array) - 1
+
+    while left <= right:
+        if array[left] + array[right] == targetSum:
+            return [array[left], array[right]]
+        elif array[left] + array[right] > targetSum:
+            right -= 1
+        else:  # array[left] + array[right] < targetSum
+            left += 1
+    return []
+
+
+# 2) VALIDATE SUBSEQUENCE
+# Solution 1
+# runTime(n) with 1 space-time
+def isValidSubsequence(array, sequence):
+    indexOfSub = 0
+
+    for element in array:
+        if sequence[indexOfSub] == element:
+            indexOfSub += 1
+
+        if indexOfSub == len(sequence):
+            return True
+
+            # return indexOfSub == len(sequence)
+    return False
+
+
+# Solution 2
+# Using while loop, potentially shorter than for loop method since it stops once it find it
+# runTime(n) with 1 space-time
+def isValidSubsequence2(array, sequence):
+    seqIndex = 0
+    arrayIndex = 0
+    while arrayIndex < len(array) and seqIndex < len(sequence):
+
+        if array[arrayIndex] == sequence[seqIndex]:
+            seqIndex += 1
+
+        arrayIndex += 1
+
+    return seqIndex == len(sequence)
+
+
+# 3) SORTED SQUARED ARRAY
+# runtime O(n)
+def sortedSquaredArray(array):
+    left = 0
+    right = len(array) - 1
+    squaredArray = []
+
+    while left <= right:
+        leftValue = abs(array[left]) ** 2
+        rightValue = abs(array[right]) ** 2
+
+        if leftValue == rightValue or rightValue > leftValue:
+            squaredArray.insert(0, rightValue)
+            right -= 1
+
+        else:  # (leftValue>rightValue)
+            squaredArray.insert(0, leftValue)
+            left += 1
+
+    return squaredArray
+
+
+# 4) TOURNAMENT WINNER
+# O(n) time and O(k) space
+HOME_TEAM_WON = 1
+def tournamentWinner(competitions, results):
+    bestTeam = ""
+    scores = {bestTeam: 0}
+
+    # loop through competitions array, enumerate keeps track of index
+    for idx, competition in enumerate(competitions):
+        result = results[idx]
+
+        homeTeam, awayTeam = competition  # Sets hometeam and awayTeam
+
+        if result == HOME_TEAM_WON:  # Determines team that won
+            winningTeam = homeTeam
+        else:
+            winningTeam = awayTeam
+
+        UpdateScores(winningTeam, scores, 3)  # Updates score of winning team
+        if scores[bestTeam] < scores[winningTeam]:  # compares for the best team
+            bestTeam = winningTeam
+
+    return bestTeam
+
+
+# Updates the winning team score or adds winning team into scores list
+def UpdateScores(team, scores, points):
+    if team in scores:
+        scores[team] += points
+    else:
+        scores[team] = points
+
+
+# 5) NON-CONSTRUCTABLE CHANGE
+# O(nlogn) runtime and O(1) or O(n) space
+def nonConstructibleChange(coins):
+    coins.sort()
+
+    possibleChange = 0
+
+    for coin in coins:
+        if coin > possibleChange + 1:
+            return possibleChange + 1
+
+        possibleChange += coin
+
+    return possibleChange + 1
+
+
+# 6) TRANSPOSE MATRIX
+# O(w*h) runtime and O(w*h) space
+def transposeMatrix(matrix):
+    # row and column values are swapped
+    transposedMatrix = []
+    for col in range(len(matrix[0])):
+        newRow = []
+        for row in range(len(matrix)):
+            newRow.append(matrix[row][col])
+
+        transposedMatrix.append(newRow)
+
+    return transposedMatrix
